3d707832c4c629176e9d38d9c2b2281d5fc87746
WebpageCoder53 <133002197+WebpageCoder53@users.noreply.github.com>
2023-11-15 17:51:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// https://teachablemachine.withgoogle.com/models/5ggpVhDNU/
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='" + data_uri + "'>";
+    });
+}
+
+console.log("ml5 version " + ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/5ggpVhDNU/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model loaded!");
+}
